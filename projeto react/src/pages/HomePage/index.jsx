649ed54d4a1a8575705a7d993c0bc00cc29741e4
lorenzo7377 <lorenzodavinci@hotmail.com>
2023-07-21 18:18:35 -0300

    Feito o primeiro título

--- projeto react/src/pages/HomePage/index.jsx
+++ projeto react/src/pages/HomePage/index.jsx
@@ -1,7 +1,14 @@
+import styles from "./styles.module.css";
+
 export default function HomePage() {
   return (
-    <div className="grey">
-      <h1>Hello world</h1>
+    <div id={styles.box}>
+      <div id={styles.caixa}>
+        <h1 id={styles.fazer}>A fazer</h1>
+        <button id={styles.primeirobotao}>+</button>
+        <button id={styles.segundobotao}>+</button>
+        <button id={styles.terceirobotao}>+</button>
+      </div>
     </div>
   );
 }
